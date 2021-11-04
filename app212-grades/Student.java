a47fc78107048f547d8bada673e99d0f4ebcc5f2
CoryLMartin <91318738+CoryLMartin@users.noreply.github.com>
2021-11-04 19:17:16 +0000

    Updated the course, modules and student, enrolled them on course

--- app212-grades/Student.java
+++ app212-grades/Student.java
@@ -4,8 +4,8 @@ import java.util.*;
  * It holds the student details relevant in our context.
  * 
  * @author Michael Kölling and David Barnes
- * Modified by Derek Peacock & Nicholas Day
- * @version 2021-08-18
+ * Modified by Cory Martin
+ * @version 1.0 2021-08-18 
  */
 public class Student
 {
@@ -24,7 +24,7 @@ public class Student
      */
     public Student()
     {
-        this("Derek", 12345678);
+        this("Cory", 22045469);
     }
     
     /**
